6decfb6817961c900a5d13158a17a6d1f028c44d
Mike Le <Mike.lemanh@gmail.com>
2026-08-07 17:55:06 +0930

    Implement climbing stairs solution using dynamic programming

---
+++ climbingStairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        int[] memo = new int[n + 1];
+        memo[1] = 1;
+        memo[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            memo[i] = memo[i - 2] + memo[i - 1];
+        }
+
+        return memo[n];
+        
+    }
+}
